eed3ba7384475137f145745f40479d457d641072
Tasfia Islam <tasf1a027@gmail.com>
2021-01-11 18:47:15 +0600

    Added JWT tokens

--- accounts/accounts/settings.py
+++ accounts/accounts/settings.py
@@ -128,5 +128,9 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
