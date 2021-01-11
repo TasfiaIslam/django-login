a9f39b94d7d9643186aa208bc7f442c201af4589
Tasfia Islam <tasf1a027@gmail.com>
2021-01-11 20:43:35 +0600

    Added Header,Footer with Routing

--- accounts/frontend/src/index.js
+++ accounts/frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import App from "./App";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+      <Footer />
+    </React.StrictMode>
+  </Router>,
   document.getElementById("root")
 );
